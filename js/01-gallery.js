ac86763d9e2725b551eea78f70e71c47b22186c3
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-09-04 15:07:53 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+let instance;
 
-console.log(galleryItems);
+const renderImages = arr => {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+        `
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+};
+
+const handleClick = event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const imageUrl = event.target.dataset.source;
+  instance = basicLightbox.create(
+    `<img width="1400" height="900" src='${imageUrl}'>`
+  );
+  instance.show();
+
+  // Створюємо случаха подій
+  gallery.addEventListener('keydown', handleClose);
+};
+
+const handleClose = event => {
+  if (event.key === 'Escape') {
+    instance.close();
+
+    // Видаляємо слухача подій після того,
+    // як закрили картинку
+    gallery.removeEventListener('keydown', handleClose);
+  }
+};
+
+renderImages(galleryItems);
+gallery.addEventListener('click', handleClick);
